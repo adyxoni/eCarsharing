bd29ff68762e5a310e12e1c0566b25252b05e6d6
ajla.sisic <ajla.sisic@stu.ibu.edu.ba>
2022-07-08 17:24:44 +0200

    improvements

---
+++ api/dao/AccountDao.class.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__.'/BaseDao.class.php';
+
+class VehiclesDao extends BaseDao{
+
+
+  public function __construct(){
+    parent::__construct("vehicles");
+  }
+
+  public function get_accounts($search, $offset, $limit, $order="-id"){
+  list($order_column, $order_direction) = self::parse_order($order);
+     return $this->query("SELECT *
+                         FROM accounts
+                         WHERE LOWER(username) LIKE CONCAT('%',:username,'%')
+                         ORDER BY ${order_column} ${order_direction}
+                         LIMIT ${limit} OFFSET ${offset}",
+                         ["username"=>$search]);
+ }
+
+}
--- api/index.php
+++ api/index.php
@@ -3,13 +3,38 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+Flight::set('flight.log_errors',TRUE);
+/*error handling for API
+Flight::map('error', function(Exception $ex){
+Flight::json(["message" => $ex->getMessage()], $ex->getCode() ? $ex->getCode() : 500);
+});
+/* utility function for reading query parameters from URL */
+Flight::map('query',function($name, $default_value=NULL){
+$request= Flight::request();
+$query_parameter= @$request->query->getData()[$name];
+$query_parameter= $query_parameter ? $query_parameter : $default_value;
+return $query_parameter;
+});
+/* utility function for getting header parameters */
+Flight::map('header', function($name){
+  $headers = getallheaders();
+  return @$headers[$name];
+});
+/* utility function for generating JWT token */
+Flight::map('jwt', function($user){
+  $jwt = \Firebase\JWT\JWT::encode(["exp" => (time() + Config::JWT_TOKEN_TIME), "id" => $user["id"], "aid" => $user["accountID"], "r" => $user["role"]], Config::JWT_SECRET);
+  return ["token" => $jwt];
+});
 
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/services/VehicleService.class.php';
 
 Flight::register('vehicleService', 'VehicleService');
+Flight::register('accountService', 'AccountService');
+Flight::register('userService', 'UserService');
 
 require_once __DIR__.'/routes/VehiclesRoutes.php';
+require_once __DIR__.'/routes/UserRoutes.php';
 
 Flight::start();
 ?>
